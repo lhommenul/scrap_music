8e86913363aece90c0afa26d933ebb59746a72ff
lhommenul <idlhommenul@gmail.com>
2021-06-22 11:23:50 +0200

    scrpper updated

--- index.js
+++ index.js
@@ -6,21 +6,14 @@ const fs =require('fs')
 
 const search = "dsq" 
 axios({
-    url:`https://www.discogs.com/fr/Anthony-Pasquarosa-VDSQ-Solo-Acoustic-Volume-Seven/release/5621216`,
+    url:`https://www.discogs.com/search/?q=tyler&type=all`,
     method:"GET"
 })
 .then(data=>{
-    // console.log(data);
     const $ = cheerio.load(data.data);
-    fs.writeFile('html.html',data.data,()=>{
-
-    })
-    // console.log($('.default-image-as-icon').attr('href'));
-    // console.log($('.thumbnail_link'));
-    $('a').each(e=>{
+    $('.thumbnail_link').each(function() {
         console.log($(this).attr('href'));
     })
-    // res.send(data.data)
 })
 .catch(err=>{
     // res.send(err)
